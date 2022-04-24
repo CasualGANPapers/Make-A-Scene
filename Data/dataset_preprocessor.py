c97a31545f81948485944ba0128085cf499c2745
VP <ivan-perunov@ya.ru>
2022-04-24 22:50:29 +0300

    Added conceptual dataset, different preprocessing results separated.

--- Data/dataset_preprocessor.py
+++ Data/dataset_preprocessor.py
@@ -14,6 +14,8 @@ from hydra.utils import instantiate
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+from urllib.request import urlretrieve
+
 try:
     from .preprocessors import Detectron2, HumanParts, HumanFace, get_edges
 except ModuleNotFoundError as e:
@@ -35,6 +37,7 @@ class PreprocessedDataset(Dataset):
         proc_total=1,
     ):
         self.name = name
+        self.image_dirs = image_dirs
 
         # Preprocessing modules, required only during preprocessing or online-processing
         self.detectron2 = detectron2
@@ -56,25 +59,17 @@ class PreprocessedDataset(Dataset):
         else:
             assert os.path.isdir(preprocessed_folder)
 
-        img_names_path = os.path.join(preprocessed_folder, f"img_names_{name}.npz")
-        if os.path.exists(img_names_path) and not force_preprocessing:
-            self.img_names = np.load(img_names_path)["img_names"]
-        else:
-            self.parse_image_names(image_dirs)
-            self.img_names = np.load(img_names_path)["img_names"]
+        self.img_names_path = os.path.join(preprocessed_folder, f"img_names_{name}.npz")
+        if not os.path.exists(self.img_names_path) or force_preprocessing:
+            self.parse_image_names()
+
+        img_list = np.load(self.img_names_path)
+        self.img_names = img_list["img_names"]
+        if "img_urls" in img_list:
+            self.img_urls = img_list["img_urls"]
 
         if not os.path.exists(preprocessed_folder) or force_preprocessing:
             self.preprocess_dataset()
-
-    def parse_image_names(self, img_dirs):
-        img_names = []
-        for directory in img_dirs:
-            for filename in os.listdir(os.path.join(self.root, directory)):
-                if os.path.splitext(filename)[1] in [".jpg", ".png"]:
-                    img_names.append(os.path.join(directory, filename))
-        np.savez(
-            os.path.join(self.preprocessed_folder, f"img_names_{self.name}"), img_names=img_names
-        )
 
     def preprocess_dataset(self):
         assert torch.cuda.is_available(), "GPU required for preprocessing"
@@ -112,9 +107,7 @@ class PreprocessedDataset(Dataset):
                 os.remove(f"tmp_names_{self.name}_{proc_id}.npz")
                 self.img_names.extend(names)
             print(len(self.img_names))
-            np.savez(
-                os.path.join(self.preprocessed_folder, f"img_names_{self.name}"), img_names=self.img_names
-            )
+            np.savez( self.img_names_path, img_names=self.img_names)
 
     def preprocess_single_process(self, proc_id,):
         correct_names = []
@@ -136,73 +129,101 @@ class PreprocessedDataset(Dataset):
         np.savez(f"tmp_names_{proc_id}", names=correct_names)
 
     def preprocess_single_image(self, img_name, proc_id=None, correct_img_names=None):
-        detectron2 = self.detectron2 if proc_id is None else self.detectron2[proc_id]
-        human_parsing = (
-            self.human_parsing if proc_id is None else self.human_parsing[proc_id]
-        )
-        human_face = self.human_face if proc_id is None else self.human_face[proc_id]
-
         image = cv2.imread(os.path.join(self.root, img_name))
-        #if image.shape[0]<256 or image.shape[1]<256:
-        #    return False
-
-        seg_panoptic, box_thing = detectron2(image).to(torch.int32).cpu().numpy()
-        seg_human = human_parsing(image).to(torch.int32).cpu().numpy()
-        seg_face, box_face = human_face(image)
-        seg_edges = get_edges(seg_panoptic, seg_human, seg_face)
-
-        seg_panoptic = np.array(seg_panoptic // 1000, dtype=np.uint8)
-        seg_human = np.array(seg_human, dtype=np.uint8)
-        seg_face = np.array(seg_face, dtype=np.uint8)
-        seg_edges = np.array(seg_edges, dtype=np.bool)
-
-        save_path = os.path.join(
-            self.preprocessed_folder, "segmentations", os.path.splitext(img_name)[0]
-        )
-        try:
+        # Detectron2 panoptic segmentation
+        detectron2 = self.detectron2 if proc_id is None else self.detectron2[proc_id]
+        if detectron2 is not None:
+            seg_panoptic, box_thing = detectron2(image).to(torch.int32).cpu().numpy()
+            seg_edges = get_edges(seg_panoptic)
+            seg_panoptic = np.array(seg_panoptic // 1000, dtype=np.uint8)
+            seg_edges = np.array(seg_edges, dtype=np.bool)
+            save_path = os.path.join(
+                self.preprocessed_folder, "segmentations", os.path.splitext(img_name)[0] + "_panoptic"
+            )
             np.savez_compressed(
                 save_path,
                 seg_panoptic=seg_panoptic,
                 box_thing=box_thing,
-                seg_human=seg_human,
-                seg_face=seg_face,
-                box_face=box_face,
-                seg_edges=seg_edges,
+                edges=seg_edges,
             )
-        except FileNotFoundError:
-            os.makedirs(os.path.dirname(save_path))
+
+        # Human parts
+        human_parsing = (
+            self.human_parsing if proc_id is None else self.human_parsing[proc_id]
+        )
+        if human_parsing is not None:
+            seg_human = human_parsing(image).to(torch.int32).cpu().numpy()
+            seg_edges = get_edges(seg_human)
+            seg_human = np.array(seg_human, dtype=np.uint8)
+            seg_edges = np.array(seg_edges, dtype=np.bool)
+            save_path = os.path.join(
+                self.preprocessed_folder, "segmentations", os.path.splitext(img_name)[0] + "_human"
+            )
             np.savez_compressed(
                 save_path,
-                seg_panoptic=seg_panoptic,
                 seg_human=seg_human,
+                edges=seg_edges,
+            )
+
+        # Human face alignment
+        human_face = self.human_face if proc_id is None else self.human_face[proc_id]
+        if human_face is not None:
+            seg_face, box_face = human_face(image)
+            seg_face = np.array(seg_face, dtype=np.uint8)
+            save_path = os.path.join(
+                self.preprocessed_folder, "segmentations", os.path.splitext(img_name)[0] + "_face"
+            )
+            np.savez_compressed(
+                save_path,
                 seg_face=seg_face,
-                seg_edges=seg_edges,
+                box_face=box_face,
             )
         return True
 
     def load_segmentation(self, img_name):
-        data = np.load(
+        # Panoptic
+        data_panoptic = np.load(
             os.path.join(
                 self.preprocessed_folder,
                 "segmentations",
-                os.path.splitext(img_name)[0] + ".npz",
+                os.path.splitext(img_name)[0] + "_panoptic.npz",
             )
         )
         seg_panoptic = F.one_hot(
-            torch.from_numpy(data["seg_panoptic"] + 1).to(torch.long), num_classes=134
+            torch.from_numpy(data_panoptic["seg_panoptic"] + 1).to(torch.long), num_classes=134
         )[..., 1:]
+        # Human parts
+        data_human = np.load(
+            os.path.join(
+                self.preprocessed_folder,
+                "segmentations",
+                os.path.splitext(img_name)[0] + "_human.npz",
+            )
+        )
         seg_human = F.one_hot(
-            torch.from_numpy(data["seg_human"] + 1).to(torch.long), num_classes=21
+            torch.from_numpy(data_human["seg_human"] + 1).to(torch.long), num_classes=21
         )[..., 1:]
+
+        #Face
+        data_face = np.load(
+            os.path.join(
+                self.preprocessed_folder,
+                "segmentations",
+                os.path.splitext(img_name)[0] + "_face.npz",
+            )
+        )
         seg_face = F.one_hot(
-            torch.from_numpy(data["seg_face"]).to(torch.long), num_classes=6
+            torch.from_numpy(data_face["seg_face"]).to(torch.long), num_classes=6
         )[..., 1:]
-        seg_edges = torch.from_numpy(data["seg_edges"]).unsqueeze(-1)
+
+        edges_panoptic = torch.from_numpy(data_panoptic["edges"]).unsqueeze(-1)
+        edges_human = torch.from_numpy(data_human["edges"]).unsqueeze(-1)
+        seg_edges = ((edges_panoptic + edges_human)>0).float()
 
         seg_map = torch.cat(
             [seg_panoptic, seg_human, seg_face, seg_edges], dim=-1
         )#.permute(2, 0, 1)
-        return seg_map, data["box_thing"], data["box_face"]
+        return seg_map, data_panoptic["box_thing"], data_face["box_face"]
 
     def __getitem__(self, idx):
         img_name = self.img_names[idx]
@@ -211,18 +232,38 @@ class PreprocessedDataset(Dataset):
         except FileNotFoundError:
             self.preprocess_single_image(img_name)
             segmentation, box_thing, box_face = self.load_segmentation(img_name)
-        image = self.get_image(img_name)
+        image = self.get_image(idx)
         data = self.transforms(image=image, mask=segmentation.numpy(), bboxes=box_thing, bboxes0=box_face)
         return data["image"], data["mask"].float(), data["bboxes"], data["bboxes0"]
 
-    def get_image(self, img_name):
+    def get_image(self, idx):
+        raise NotImplementedError
+
+    def parse_image_names(self):
         raise NotImplementedError
 
     def __len__(self):
         return len(self.img_names)
 
 
-class COCO2014Dataset(PreprocessedDataset):
+class BaseCOCODataset(PreprocessedDataset):
+    def get_image(self, idx):
+        img_name = self.img_names[idx]
+        path = os.path.join(self.root, img_name)
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+
+    def parse_image_names(self):
+        img_names = []
+        for directory in self.img_dirs:
+            for filename in os.listdir(os.path.join(self.root, directory)):
+                if os.path.splitext(filename)[1] in [".jpg", ".png"]:
+                    img_names.append(os.path.join(directory, filename))
+        np.savez(self.img_names_path, img_names=img_names)
+
+
+class COCO2014Dataset(BaseCOCODataset):
     def __init__(self, root, preprocessed_folder, **kwargs):
         super().__init__(
             "coco2014"
@@ -232,14 +273,8 @@ class COCO2014Dataset(PreprocessedDataset):
             **kwargs,
         )
 
-    def get_image(self, img_name):
-        path = os.path.join(self.root, img_name)
-        image = cv2.imread(path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image
 
-
-class COCO2017Dataset(PreprocessedDataset):
+class COCO2017Dataset(BaseCOCODataset):
     def __init__(self, root, preprocessed_folder, **kwargs):
         super().__init__(
             "coco2017"
@@ -249,11 +284,49 @@ class COCO2017Dataset(PreprocessedDataset):
             **kwargs,
         )
 
-    def get_image(self, img_name):
+
+class Conceptual12mDataset(PreprocessedDataset):
+    def __init__(self, root, preprocessed_folder, **kwargs):
+        super().__init__(
+            "cc12m"
+            root=root,
+            image_dirs=["cc12m_images"],
+            preprocessed_folder=preprocessed_folder,
+            **kwargs,
+        )
+
+    def parse_image_names(self, listfile):
+        img_names = []
+        img_urls = []
+        with open(listfile, "r") as urllist:
+            for i, line in enumerate(urllist):
+                url, caption = line.split("\t")
+                caption = caption.strip()
+                img_names.append(caption+".jpg")
+        np.savez(self.img_names_path, img_names=img_names, img_urls=img_urls)
+
+
+
+    def get_image(self, idx):
+        img_name = self.img_names[idx]
         path = os.path.join(self.root, img_name)
+        if not os.path.exists(path):
+            self.download_image(self.url[idx], img_name)
         image = cv2.imread(path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return image
+
+
+
+    def download_image(self, url, image_name):
+        try:
+            image_path = os.path.join(self.root, image_name)
+            urlretrieve(url, image_path)
+            return True
+        except HTTPError:
+            print("Failed to download the image: ", image_name)
+            return False
+
 
 class ConcatDataset(ConcatDataset_):
     def __init__(self, datasets_configs, proc_total=1, force_preprocessing=False, ):
@@ -286,6 +359,7 @@ class ConcatDataset(ConcatDataset_):
                                   human_parsing=self.human_parsing)
             datasets.append(dataset)
         super().__init__(datasets)
+
 
 if __name__ == "__main__":
     coco = COCO2014Dataset(
