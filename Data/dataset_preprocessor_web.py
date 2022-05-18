65c39e33d11a4302c0f3ede35f729f7ed60cae3c
VP <ivan-perunov@ya.ru>
2022-05-18 19:55:29 +0300

    Almost ready.

--- Data/dataset_preprocessor_web.py
+++ Data/dataset_preprocessor_web.py
@@ -17,17 +17,90 @@ from urllib.request import urlretrieve
 from webdataset import WebDataset
 from webdataset.shardlists import split_by_node
 
-class ProcessData:
-    def __init__(self):
+class PreprocessData:
+    def __init__(self, ready_queue):
         self.transforms = A.Compose([
             A.SmallestMaxSize(512),
             A.CenterCrop(512, 512, always_apply=True),
             ToTensorV2(transpose_mask=True)
         ])
+        self.lasttar = "no"
+        self.ready_queue = ready_queue
     def __call__(self, data):
         result = self.transforms(image=data["jpg"])
         data["jpg"] = result["image"]
         data["tarname"] = os.path.basename(data["__url__"])
+        if self.lasttar!=data["tarname"]:
+            rank = os.environ["RANK"]
+            worker_info = torch.utils.data.get_worker_info()
+            if worker_info is not None:
+                worker = worker_info.id
+            else:
+                worker = None
+
+            if self.lasttar != "no":
+                self.ready_queue.put("%s/%s/processed/%s" % (rank, worker, self.lasttar))
+                self.ready_queue.put("%s/%s/started/%s" % (rank, worker, data["tarname"]))
+                print(self.lasttar, "processed!")
+            self.lasttar = data["tarname"]
+        return data
+
+class UnprocessedWebDataset(WebDataset):
+    def __init__(self, root, *args, ready_queue=None, **kwargs):
+        shards = [os.path.join(root, filename) for filename in os.listdir(root) if os.path.splitext(filename)[1]==".tar"]
+        super().__init__(shards, *args, nodesplitter=split_by_node, **kwargs)
+        self.decode("rgb")
+        self.map(PreprocessData(ready_queue))
+        self.to_tuple("__key__", "tarname", "jpg")
+
+class ProcessData:
+    def __init__(self,):
+        self.transforms = A.Compose([
+            A.SmallestMaxSize(256),
+            A.RandomCrop(256, 256, always_apply=True),
+            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(transpose_mask=True)
+        ], bbox_params=A.BboxParams(format='pascal_voc', min_area=100, min_visibility=0.2),
+            additional_targets={"bboxes0": "bboxes"})
+
+    def __call__(self, data):
+        npz_data = data["npz"]
+        # Panoptic
+        seg_panoptic = F.one_hot(
+            torch.from_numpy(npz_data["seg_panoptic"] + 1).to(torch.long), num_classes=134
+        )[..., 1:]
+        edges_panoptic = torch.from_numpy(npz_data["edge_panoptic"]).unsqueeze(-1)
+        box_thing = npz_data["box_things"].tolist()
+        for box in box_thing:
+            box.append(0)
+
+        # Human parts
+        seg_human = F.one_hot(
+            torch.from_numpy(npz_data["seg_human"] + 1).to(torch.long), num_classes=21
+        )[..., 1:]
+        edges_human = torch.from_numpy(npz_data["edge_human"]).unsqueeze(-1)
+
+        # Edges
+        seg_edges = (edges_panoptic + edges_human).float()
+
+        # Face
+        seg_face = F.one_hot(
+            torch.from_numpy(npz_data["seg_face"]).to(torch.long), num_classes=6
+        )[..., 1:]
+        box_face = npz_data["box_face"].tolist()
+        for box in box_face:
+            box.append(0)
+
+        # Concatenate masks
+        seg_map = torch.cat(
+            [seg_panoptic, seg_human, seg_face, seg_edges], dim=-1
+        ).numpy()
+
+        transformed_data = self.transforms(image=data["jpg"], bboxes=box_thing, bboxes0=box_face,)
+        data["jpg"] = transformed_data["image"]
+        data["mask"] = seg_map
+        data["box_things"] = transformed_data["bboxes"]
+        data["box_face"] = transformed_data["bboxes0"]
         return data
 
 class PreprocessedWebDataset(WebDataset):
@@ -35,194 +108,9 @@ class PreprocessedWebDataset(WebDataset):
         shards = [os.path.join(root, filename) for filename in os.listdir(root) if os.path.splitext(filename)[1]==".tar"]
         super().__init__(shards, *args, nodesplitter=split_by_node, **kwargs)
         self.decode("rgb")
+        #self.decode("npz")
         self.map(ProcessData())
-        self.to_tuple("__key__", "tarname", "jpg")
-
-class PreprocessedDataset(Dataset):
-    def __init__(
-            self,
-            root=None,
-            image_dirs=None,
-            preprocessed_folder=None,
-    ):
-        self.image_dirs = image_dirs
-
-        self.preprocessed_folder = preprocessed_folder
-        self.preprocessed_path = os.path.join(preprocessed_folder, "segmentations", "%s_%s.npz", )
-
-        self.root = root
-        self.transforms = A.Compose([
-            A.SmallestMaxSize(256),
-            A.RandomCrop(256, 256, always_apply=True),
-            # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(transpose_mask=True)
-        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=["class_labels"], min_area=100, min_visibility=0.2),
-            additional_targets={"bboxes0": "bboxes"})
-
-        if not os.path.exists(preprocessed_folder):
-            os.makedirs(preprocessed_folder)
-        else:
-            assert os.path.isdir(preprocessed_folder)
-
-        self.img_names_path = os.path.join(preprocessed_folder, f"img_names_{self.name}.npz")
-        if not os.path.exists(self.img_names_path):
-            self.parse_image_names()
-
-        img_list = np.load(self.img_names_path)
-        self.img_names = img_list["img_names"]
-        if "img_urls" in img_list:
-            self.img_urls = img_list["img_urls"]
-
-    def load_segmentation(self, idx):
-        img_name = os.path.splitext(self.img_names[idx])[0]
-
-        data_panoptic = np.load(self.preprocessed_path % (img_name, "panoptic"))
-        data_human = np.load(self.preprocessed_path % (img_name, "human"))
-        data_face = np.load(self.preprocessed_path % (img_name, "face"))
-
-        # Panoptic
-        seg_panoptic = F.one_hot(
-            torch.from_numpy(data_panoptic["seg_panoptic"] + 1).to(torch.long), num_classes=134
-        )[..., 1:]
-        edges_panoptic = torch.from_numpy(data_panoptic["edges"]).unsqueeze(-1)
-        box_thing = data_panoptic["box_things"]
-
-        # Human parts
-        seg_human = F.one_hot(
-            torch.from_numpy(data_human["seg_human"] + 1).to(torch.long), num_classes=21
-        )[..., 1:]
-        edges_human = torch.from_numpy(data_human["edges"]).unsqueeze(-1)
-
-        # Edges
-        seg_edges = (edges_panoptic + edges_human).float()
-
-        # Face
-        seg_face = F.one_hot(
-            torch.from_numpy(data_face["seg_face"]).to(torch.long), num_classes=6
-        )[..., 1:]
-        box_face = data_face["box_face"]
-
-        # Concatenate masks
-        seg_map = torch.cat(
-            [seg_panoptic, seg_human, seg_face, seg_edges], dim=-1
-        )
-
-        return np.array(seg_map), box_thing, box_face
-
-    def __getitem__(self, idx):
-        segmentation, box_thing, box_face = self.load_segmentation(idx)
-        image, _ = self.get_image(idx)
-        data = self.transforms(image=image, mask=segmentation, bboxes=box_thing, bboxes0=box_face,
-                               class_labels=np.zeros(box_thing.shape[0]))
-        return data["image"], data["mask"], data["bboxes"], data["bboxes0"], self.img_names[idx]
-
-    def get_image(self, idx):
-        raise NotImplementedError
-
-    def parse_image_names(self):
-        raise NotImplementedError
-
-    def __len__(self):
-        return len(self.img_names)
-
-
-class BaseCOCODataset(PreprocessedDataset):
-    def get_image(self, idx):
-        img_name = self.img_names[idx]
-        path = os.path.join(self.root, img_name)
-        image = cv2.imread(path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image, img_name
-
-    def parse_image_names(self):
-        img_names = []
-        for directory in self.image_dirs:
-            for filename in os.listdir(os.path.join(self.root, directory)):
-                if os.path.splitext(filename)[1] in [".jpg", ".png"]:
-                    img_names.append(os.path.join(directory, filename))
-        np.savez(self.img_names_path, img_names=img_names)
-
-
-class COCO2014Dataset(BaseCOCODataset):
-    name = "coco2014"
-    image_dirs = "train2014"
-
-    def __init__(self, root, preprocessed_folder, **kwargs):
-        super().__init__(
-            root=root,
-            image_dirs=["train2014"],
-            preprocessed_folder=preprocessed_folder,
-            **kwargs,
-        )
-
-
-class COCO2017Dataset(BaseCOCODataset):
-    name = "coco2017"
-    image_dirs = "train2017"
-
-    def __init__(self, root, preprocessed_folder, **kwargs):
-        super().__init__(
-            root=root,
-            image_dirs=["train2017"],
-            preprocessed_folder=preprocessed_folder,
-            **kwargs,
-        )
-
-
-class Conceptual12mDataset(PreprocessedDataset):
-    name = "cc12m"
-
-    def __init__(self, root, preprocessed_folder, **kwargs):
-        super().__init__(
-            root=root,
-            **kwargs,
-        )
-
-    def parse_image_names(self, listfile):
-        img_names = []
-        img_urls = []
-        with open(listfile, "r") as urllist:
-            for i, line in enumerate(urllist):
-                url, caption = line.split("\t")
-                caption = caption.strip()
-                img_names.append(caption + ".jpg")
-        np.savez(self.img_names_path, img_names=img_names, img_urls=img_urls)
-
-    def get_image(self, idx):
-        img_name = self.img_names[idx]
-        path = os.path.join(self.root, img_name)
-        if not os.path.exists(path):
-            self.download_image(self.url[idx], img_name)
-        image = cv2.imread(path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image, img_name
-
-    def download_image(self, url, image_name):
-        try:
-            image_path = os.path.join(self.root, image_name)
-            urlretrieve(url, image_path)
-            return True
-        except HTTPError:
-            print("Failed to download the image: ", image_name)
-            return False
-
-
-class ConcatDataset(ConcatDataset_):
-    def get_true_idx(self, idx):
-        if idx < 0:
-            if -idx > len(self):
-                raise ValueError("absolute value of index should not exceed dataset length")
-            idx = len(self) + idx
-        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
-        if dataset_idx == 0:
-            sample_idx = idx
-        else:
-            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
-        return dataset_idx, sample_idx
-
-    def get_image(self, idx):
-        dataset_idx, sample_idx = self.get_true_idx(idx)
-        return self.datasets[dataset_idx].get_image(sample_idx)
+        self.to_tuple("jpg", "mask", "box_things", "box_face", "txt")
 
 
 if __name__ == "__main__":
--- Data/preprocessor_web.py
+++ Data/preprocessor_web.py
@@ -8,6 +8,7 @@ from .preprocessors import HumanFace
 from tqdm import tqdm
 import numpy as np
 import hydra
+from webdataset import WebDataset, TarWriter
 
 
 class WebPreprocessor:
@@ -34,8 +35,12 @@ class WebPreprocessor:
         self.preprocessed_folder = preprocessed_folder
         self.preprocessed_path = os.path.join(
             preprocessed_folder,
-            "segmentations",
-            f"%s_%s.npz",
+            "untars",
+            f"%s/%s_%s.npz",
+        )
+        self.repacked_path = os.path.join(
+            preprocessed_folder,
+            "tars",
         )
         self.log_path = os.path.join(
             preprocessed_folder,
@@ -47,6 +52,7 @@ class WebPreprocessor:
         assert torch.cuda.is_available(), "GPU required for preprocessing"
         procs = []
         mp.set_start_method("spawn")
+        ready_queue = mp.Queue()
         for proc_type in self.proc_per_gpu:
             devices = self.devices * self.proc_per_gpu[proc_type]
             n_cpus = self.proc_per_cpu[proc_type]
@@ -61,6 +67,7 @@ class WebPreprocessor:
                         self.idx * proc_per_machine + proc_id,
                         dev_id,
                         proc_total,
+                        ready_queue,
                     ),
                 )
                 p.start()
@@ -74,17 +81,20 @@ class WebPreprocessor:
                         self.idx * proc_per_machine + len(devices) + proc_id,
                         "cpu",
                         proc_total,
+                        ready_queue,
                     ),
                 )
                 p.start()
                 procs.append(p)
+
+        self.repacker_process(ready_queue, proc_per_machine)
         for proc in procs:
             proc.join()
 
-    def preprocess_single_process(self, proc_type, proc_id, dev_id, proc_total):
+    def preprocess_single_process(self, proc_type, proc_id, dev_id, proc_total, ready_queue):
         os.environ["RANK"] = str(proc_id)
         os.environ["WORLD_SIZE"] = str(proc_total)
-        dataset = hydra.utils.instantiate(self.dataset)
+        dataset = hydra.utils.instantiate(self.dataset, ready_queue=ready_queue)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_workers)
         correct_names = []
         if dev_id != "cpu":
@@ -101,12 +111,70 @@ class WebPreprocessor:
             x = 0
             for batch in tqdm(dataloader, file=logfile):
                 imgnames, tarnames, images = batch
-                print(proc_id, proc_total, imgnames, tarnames)
-                break
-                data = processor(images)
-                save_path = self.preprocessed_path % (tarname, image_name, proc_type)
-                self.check_path(save_path)
-                np.savez(save_path, **data)
+                batched_data = processor(images)
+                #print(proc_id, proc_total, imgnames, tarnames)
+                #print(batched_data["box_things"])
+                for i in range(len(imgnames)):
+                    tarname, imgname = tarnames[i], imgnames[i]
+                    save_path = self.preprocessed_path % (tarname, imgname, proc_type)
+                    self.check_path(save_path)
+                    data = {key: batched_data[key][i] for key in batched_data}
+                    np.savez(save_path, **data)
+        ready_queue.put("%s/und/done/und" % proc_id)
+
+    def repacker_process(self, ready_queue, proc_per_machine):
+        proc_done = 0
+        info = {str(i): {} for i in  range(proc_per_machine)}
+        while proc_done < proc_per_machine:
+            command = ready_queue.get()
+            proc_id, worker, state, tarname = command.split("/")
+            info[tarname] = 0 if tarname not in info else info[tarname]
+            if state == "done":
+                proc_done += 1
+                for worker in info[proc_id]:
+                    tarname = info[proc_id][worker]
+                    info[tarname] += 1
+                    if info[tarname] == 3:
+                        self.repack_single_tar(tarname)
+            elif state == "started":
+                info[proc_id][worker] = tarname 
+            elif state == "processed":
+                info[tarname] += 1
+                if info[tarname] == 3:
+                    self.repack_single_tar(tarname)
+
+
+        print("Processed all data!")
+
+    def repack_single_tar(self, tarname):
+        old_data = WebDataset(os.path.join(self.dataset.root, tarname))
+        new_path = os.path.join(self.repacked_path, tarname)
+        self.check_path(new_path)
+        new_data = TarWriter(new_path)
+        for sample in old_data:
+            new_sample = {}
+            imgname = new_sample["__key__"] = sample["__key__"]
+            new_sample["jpg"] = sample["jpg"]
+            new_sample["txt"] = sample["txt"]
+
+            data_face = np.load(self.preprocessed_path % (tarname, imgname, "face"), allow_pickle=True)
+            data_human = np.load(self.preprocessed_path % (tarname, imgname, "human"))
+            data_panoptic = np.load(self.preprocessed_path % (tarname, imgname, "panoptic"))
+
+            data_merged = {}
+            data_merged["seg_panoptic"] = data_panoptic["seg_panoptic"]
+            data_merged["edge_panoptic"] = data_panoptic["edges"]
+            data_merged["box_things"] = data_panoptic["box_things"]
+            data_merged["seg_human"] = data_human["seg_human"]
+            data_merged["edge_human"] = data_human["edges"]
+            data_merged["seg_face"] = data_face["seg_face"]
+            data_merged["box_face"] = data_face["box_face"]
+            new_sample["npz"] = data_merged
+
+            new_data.write(new_sample)
+        new_data.close()
+
+
 
     def check_path(self, path):
         dirname = os.path.dirname(path)
