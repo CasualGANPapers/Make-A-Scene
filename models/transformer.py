8fa2d9ab477ad334566b6b1de283da0c06ae83c2
Dominic Rampas <61938694+dome272@users.noreply.github.com>
2022-06-14 21:59:39 +0200

    Add files via upload

--- models/transformer.py
+++ models/transformer.py
@@ -22,7 +22,7 @@ class SelfAttention(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_attn_heads = num_attn_heads
         self.d = math.sqrt(self.hidden_dim // self.num_attn_heads)
-        self.qkv = nn.Linear(hidden_dim, 3*hidden_dim)
+        self.qkv = nn.Linear(hidden_dim, 3 * hidden_dim)
         self.attn_drop = nn.Dropout(attn_dropout_prob)
 
         self.out_proj = nn.Linear(hidden_dim, hidden_dim)
@@ -57,8 +57,10 @@ class SelfAttention(nn.Module):
         if self.cogview_pb_relax and not self.rudalle_relax:
             alpha = 32
             attn_scores_scaled = attn_scores / alpha
-            attn_scores_scaled_max, _ = attn_scores_scaled.detach().view([attn_scores.shape[0], attn_scores.shape[1], -1]).max(dim=-1)
-            attn_scores_scaled_max = attn_scores_scaled_max[..., None, None].expand([-1, -1, attn_scores.size(2), attn_scores.size(3)])
+            attn_scores_scaled_max, _ = attn_scores_scaled.detach().view(
+                [attn_scores.shape[0], attn_scores.shape[1], -1]).max(dim=-1)
+            attn_scores_scaled_max = attn_scores_scaled_max[..., None, None].expand(
+                [-1, -1, attn_scores.size(2), attn_scores.size(3)])
             attn_scores = (attn_scores_scaled - attn_scores_scaled_max) * alpha
         return attn_scores
 
@@ -88,7 +90,8 @@ class SelfAttention(nn.Module):
         else:
             context = torch.matmul(attn_probs, v)
 
-        context = context.permute(0, 2, 1, 3).view(context.shape[0], context.shape[1], context.shape[2]*context.shape[3])
+        context = context.permute(0, 2, 1, 3).view(context.shape[0], context.shape[1],
+                                                   context.shape[2] * context.shape[3])
 
         if self.rudalle_relax:
             scale = context.detach().max().item()
@@ -112,8 +115,8 @@ class MLP(nn.Module):
                  rudalle_relax=False
                  ):
         super(MLP, self).__init__()
-        self.lin1 = nn.Linear(hidden_dim, 4*hidden_dim)
-        self.lin2 = nn.Linear(4*hidden_dim, hidden_dim)
+        self.lin1 = nn.Linear(hidden_dim, 4 * hidden_dim)
+        self.lin2 = nn.Linear(4 * hidden_dim, hidden_dim)
         self.dropout = nn.Dropout(dropout_prob)
         self.rudalle_relax = rudalle_relax
 
@@ -185,7 +188,8 @@ class TransformerLayer(nn.Module):
             ln_out = self.ln_out(x)
 
         if use_cache and cached:
-            mlp_out = torch.cat((cache[-1] if mlp_cache else ln_out[..., :cached, :], self.mlp(ln_out[..., :cached, :])), dim=-2)
+            mlp_out = torch.cat(
+                (cache[-1] if mlp_cache else ln_out[..., :cached, :], self.mlp(ln_out[..., :cached, :])), dim=-2)
             if mlp_cache:
                 new_cache = new_cache + (mlp_out,)
         else:
@@ -199,20 +203,21 @@ class TransformerLayer(nn.Module):
         return x, new_cache
 
 
-class GPT(nn.Module):
+class Transformer(nn.Module):
     def __init__(self,
                  num_layers,
                  hidden_dim,
                  num_attn_heads,
                  attn_dropout_prop,
                  out_dropout_prob,
+                 image_tokens_per_dim=32,
                  text_length=256,
-                 image_tokens_per_dim=16,
                  cogview_pb_relax=True,
                  cogview_sandwich_layernorm=True,
                  cogview_layernorm_prescale=False,
                  rudalle_relax=False
                  ):
+        super(Transformer, self).__init__()
         self.num_layers = num_layers
         self.cogview_pb_relax = cogview_pb_relax
         self.rudalle_relax = rudalle_relax
@@ -233,12 +238,63 @@ class GPT(nn.Module):
         self.register_buffer("mask", self._create_mask(text_length, image_tokens_per_dim))
         self.final_ln = nn.LayerNorm(hidden_dim, eps=1e-5)
 
-    def _create_mask(self, text_length, image_tokens_per_dim=16):
-        size = text_length + image_tokens_per_dim**2
+    def _create_mask(self, text_length, image_tokens_per_dim=32):
+        size = text_length + image_tokens_per_dim ** 2
         return torch.tril(torch.ones(size, size, dtype=torch.float32))
 
     def get_block_size(self):
         return self.block_size
+
+    def forward(self, x, attn_mask, cache=None, use_cache=None):
+        if cache is None:
+            cache = {}
+
+        for i, layer in enumerate(self.layers):
+            mask = attn_mask
+            layer_mask = self.mask[:mask.size(2), :mask.size(3)]
+            mask = torch.mul(attention_mask, layer_mask)
+            x, layer_cache = layer(x, mask, cache.get(i), mlp_cache=i == len(self.layers) - 1, use_cache=use_cache)
+            cache[i] = layer_cache
+
+        if self.rudalle_relax:
+            ln_out = self.final_ln(x / x.detach().max(dim=-1)[0].unsqueeze(-1))
+        else:
+            ln_out = self.final_ln(x)
+
+        return ln_out, cache
+
+
+class MakeAScene(nn.Module):
+    def __init__(self,
+                 transformer_config,
+                 hidden_dim,
+                 image_vocab_size,
+                 seg_vocab_size,
+                 text_vocab_size,
+                 image_tokens_per_dim,
+                 text_length
+                 ):
+        super(MakeAScene, self).__init__()
+
+        self.transformer = Transformer(**transformer_config)
+
+        self.image_token_embedding = nn.Embedding(image_vocab_size, hidden_dim)
+        self.seg_token_embedding = nn.Embedding(seg_vocab_size, hidden_dim)
+        self.text_token_embedding = nn.Embedding(text_vocab_size, hidden_dim)
+
+        self.text_pos_embeddings = _init_weightstorch.nn.Embedding(text_length + 1, hidden_dim)
+        self.image_row_embeddings = torch.nn.Embedding(image_tokens_per_dim, hidden_dim)
+        self.image_col_embeddings = torch.nn.Embedding(image_tokens_per_dim, hidden_dim)
+        self._init_weights(self.text_pos_embeddings)
+        self._init_weights(self.image_row_embeddings)
+        self._init_weights(self.image_col_embeddings)
+
+        self.to_logits = torch.nn.Sequential(
+            torch.nn.LayerNorm(hidden_dim),  # TODO: check if this is redundant
+            torch.nn.Linear(hidden_dim, image_vocab_size + seg_vocab_size + text_vocab_size),
+        )
+
+        self.embedding_dropout = torch.nn.Dropout(embedding_dropout_prob)
 
     def _init_weights(self, module):
         if isinstance(module, (nn.Linear, nn.Embedding)):
@@ -249,22 +305,38 @@ class GPT(nn.Module):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
 
-    def forward(self, x, attn_mask, cache=None, use_cache=None):
-        if cache is None:
-            cache = {}
+    def forward(self, text_tokens, seg_tokens, img_tokens):
+        text_range = torch.arange(self.text_seq_length)
+        text_range += (self.vocab_size - self.text_seq_length)
+        text_range = text_range.to(self.device)
+        text_tokens = torch.where(text_tokens == 0, text_range, text_tokens)
+        text_pos = self.text_pos_embeddings(torch.arange(text_tokens.shape[1], device=self.device))
+        text_embeddings = self.text_embeddings(text_tokens) + text_pos
 
-        for i, layer in enumerate(self.layers):
-            mask = attn_mask
-            layer_mask = self.mask[:mask.size(2), :mask.size(3)]
-            mask = torch.mul(attention_mask, layer_mask)
-            x, layer_cache = layer(x, mask, cache.get(i), mlp_cache=i == len(self.layers)-1, use_cache=use_cache)
-            cache[i] = layer_cache
-
-        if self.rudalle_relax:
-            ln_out = self.final_ln(x / x.detach().max(dim=-1)[0].unsqueeze(-1))
+        if img_tokens is not None:
+            img_pos = self.get_image_pos_embeddings(img_tokens, past_length=0)
+            image_embeddings = self.image_embeddings(img_tokens) + img_pos
+            embeddings = torch.cat((text_embeddings, image_embeddings), dim=1)
         else:
-            ln_out = self.final_ln(x)
+            embeddings = text_embeddings
 
-        return ln_out, cache
+
+    # def forward(self, text_tokens, seg_tokens, img_tokens):
+    #     if self.training:
+    #         mask = torch.bernoulli(self.pkeep * torch.ones_like(img_tokens))
+    #         mask = mask.round().to(dtype=torch.int64)
+    #         random_tokens = torch.randint_like(img_tokens, self.vocab_size)
+    #         random_img_tokens = mask * img_tokens + (1 - mask) * random_tokens
+    #     else:
+    #         random_img_tokens = img_tokens
+    #
+    #     tokens = torch.cat([text_tokens, seg_tokens, random_img_tokens], dim=1)
+    #     target = img_tokens
+    #
+    #     logits = self.transformer(tokens[:, :-1])
+    #     logits = logits[:, text_tokens.shape[1] + seg_tokens.shape[1] - 1:]
+    #
+    #     return logits, target
+
 
 
